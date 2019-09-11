4254d749ca621938aede600d387b7c73112bb825
Felipe Machado <feelipe@email.com>
2019-09-11 16:48:17 -0300

    Exercício menu-dropdown

---
+++ menu-dropdown/js/index.js
@@ -0,0 +1,14 @@
+$(function() {
+  $("li").hover(
+    function() {
+      $(this)
+        .find(".menu-2")
+        .slideDown();
+    },
+    function() {
+      $(this)
+        .find(".menu-2")
+        .slideUp();
+    }
+  );
+});
